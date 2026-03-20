af6cda551d210c861b4f3ed02973650b0680373f
hassanelalfy3 <hassan.elalfy@gabtic.com>
2026-03-20 23:58:29 +0200

    Update EGX_Stocks.py

--- EGX_Stocks.py
+++ EGX_Stocks.py
@@ -16,7 +16,7 @@ st.markdown("""
     
     /* FIX: High-contrast Scorecards (White background, Black text) */
     [data-testid="stMetric"] {
-        background-color: #f0f2f6 !important; 
+        background-color: #ffffff !important; 
         border: 2px solid #238636;
         padding: 15px;
         border-radius: 12px;
