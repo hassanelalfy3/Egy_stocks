cef18e2ea32c468b4f590344bbaf2b0c163201f9
hassanelalfy3 <hassan.elalfy@gabtic.com>
2026-03-23 05:31:14 +0200

    Update EGX_Stocks_v2.py

--- EGX_Stocks_v2.py
+++ EGX_Stocks_v2.py
@@ -113,7 +113,7 @@ elif selected_strat == "Bollinger Band Reversal":
 
 st.sidebar.header("📊 Market Settings")
 p_tf = st.sidebar.selectbox("Timeframe", ["1m", "5m", "15m", "30m", "1h", "1d", "5d", "1M", "3M", "6M", "YTD"], index=5)
-tickers_input = st.sidebar.text_area("Tickers", "GC=F, NVDA, BTC-USD, COMI.CA, FWRY.CA, ETH-USD")
+tickers_input = st.sidebar.text_area("Tickers", "GC=F, COMI.CA, TMGH.CA, HRHO.CA, FWRY.CA, EAST.CA, ETEL.CA, ABUK.CA, ADIB.CA, EFIH.CA, ATLC.CA,AMD")
 SCAN_LIST = [t.strip().upper() for t in tickers_input.split(",") if t.strip()]
 p_interval = st.sidebar.select_slider("Refresh (Sec)", options=[30, 60, 120, 300], value=60)
 
